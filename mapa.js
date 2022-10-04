2ab875c8129cf0608914119f572096b440a9066e
proferobles <cartografia.digital@unab.edu.ar>
2022-10-04 19:59:52 -0300

    desactivar scroll en map

--- mapa.js
+++ mapa.js
@@ -1,5 +1,5 @@
 // Inicializar el mapa con una vista central (setview) y un zoom (3)
-var map = L.map('map').setView([-34.852121, -58.355234], 13 );
+var map = L.map('map').setView([-34.852121, -58.355234], 13, );
 //Agregar mapa base de IGN
 var ign_clasico = L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png', {
     attribution: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | <a href="http://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2" target="_blank">Instituto Geográfico Nacional</a> + <a href="http://www.osm.org/copyright" target="_blank">OpenStreetMap</a>',
@@ -179,6 +179,7 @@ var options = {
 		
 var control = L.Control.styledLayerControl(baseMaps, overlays, options);
 map.addControl(control);
+map.scrollWheelZoom.disable();
 
 			
 
